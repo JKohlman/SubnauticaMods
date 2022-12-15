86b88009ea2dc74580f678921181805da1f67796
Jayden Kohlman <2236351+JKohlman@users.noreply.github.com>
2022-12-14 19:48:11 -0700

    Seems to work

---
+++ NoPdaDelay.cs
@@ -0,0 +1,25 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+
+namespace NoPdaDelay
+{
+    [BepInPlugin(myGUID, pluginName, versionString)]
+    [BepInProcess("Subnautica.exe")]
+    public class NoPdaDelay : BaseUnityPlugin
+    {
+        private const string myGUID = "com.essence.NoPdaDelay";
+        private const string pluginName = "No PDA Delay";
+        private const string versionString = "1.0.0";
+
+        private static readonly Harmony harmony = new Harmony(myGUID);
+
+        public static ManualLogSource logger;
+
+        public void Awake()
+        {
+            logger = Logger;
+            harmony.PatchAll();
+        }
+    }
+}
---
+++ Patches/PDAPatch.cs
@@ -0,0 +1,35 @@
+﻿using HarmonyLib;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Reflection.Emit;
+
+namespace PDAInstaClose.Patches
+{
+    [HarmonyPatch(typeof(PDA), "Open")]
+    public static class PDAOpenPatch
+    {
+        static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+        {
+            return new CodeMatcher(instructions)
+                .MatchForward(false,
+                    new CodeMatch(i => i.opcode == OpCodes.Ldfld && ((FieldInfo)i.operand).Name == "sequence"))
+                .Advance(1)
+                .SetOperandAndAdvance(0.0f)
+                .InstructionEnumeration();
+        }
+    }
+
+    [HarmonyPatch(typeof(PDA), "Close")]
+    public static class PDAClosePatch
+    {
+        static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+        {
+            return new CodeMatcher(instructions)
+                .MatchForward(false,
+                    new CodeMatch(i => i.opcode == OpCodes.Ldfld && ((FieldInfo)i.operand).Name == "sequence"))
+                .Advance(1)
+                .SetOperandAndAdvance(0.0f)
+                .InstructionEnumeration();
+        }
+    }
+}
