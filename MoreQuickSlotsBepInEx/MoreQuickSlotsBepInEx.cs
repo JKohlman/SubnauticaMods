68d1521bab51ded501708b763b62cacba2844643
Jayden Kohlman <2236351+JKohlman@users.noreply.github.com>
2022-12-20 14:02:50 -0700

    Dynamically allocate slots when changed in game

--- MoreQuickSlotsBepInEx/MoreQuickSlotsBepInEx.cs
+++ MoreQuickSlotsBepInEx/MoreQuickSlotsBepInEx.cs
@@ -2,6 +2,7 @@
 using BepInEx.Configuration;
 using BepInEx.Logging;
 using HarmonyLib;
+using MoreQuickSlotsBepInEx.Patches;
 using System;
 using System.Collections.Generic;
 using UnityEngine;
@@ -20,7 +21,7 @@ namespace MoreQuickSlotsBepInEx
 
         public static ManualLogSource logger;
 
-        public const int MAX_EXTRA_SLOTS = 5;
+        public const int MAX_EXTRA_SLOTS = 15;
 
         private readonly KeyCode[] defaultHotkeys = { KeyCode.Alpha6, KeyCode.Alpha7, KeyCode.Alpha8, KeyCode.Alpha9, KeyCode.Alpha0, };
 
@@ -45,16 +46,17 @@ namespace MoreQuickSlotsBepInEx
                     new AcceptableValueRange<int>(0, MAX_EXTRA_SLOTS)
                 )
             );
+            CfgExtraSlots.SettingChanged += (object sender, EventArgs e) => { QuickSlots_Patch.ReDrawSlots(); };
             for (int i = 0; i < MAX_EXTRA_SLOTS; i++)
             {
                 CfgSlotHotkeys.Add(Config.Bind(
                     "Hotkeys",
                     "Quickslot " + (i + 6).ToString() + " Hotkey",
-                    new KeyboardShortcut(defaultHotkeys[i]),
+                    new KeyboardShortcut((i < defaultHotkeys.Length) ? defaultHotkeys[i] : KeyCode.None),
                     new ConfigDescription(
-                        "Hotkey for quickslot " + (i + 6).ToString(), 
+                        "Hotkey for quickslot " + (i + 6).ToString(),
                         null,
-                        new ConfigurationManagerAttributes { Order = 6 - i }
+                        new ConfigurationManagerAttributes { Order = (MAX_EXTRA_SLOTS + 1) - i }
                     )
                 ));
             }
@@ -70,7 +72,7 @@ namespace MoreQuickSlotsBepInEx
             if (Inventory.main == null)
                 return;
 
-            for (int i = 0; i < MAX_EXTRA_SLOTS; i++)
+            for (int i = 0; i < CfgExtraSlots.Value; i++)
             {
                 if (CfgSlotHotkeys[i].Value.IsDown())
                 {
--- MoreQuickSlotsBepInEx/Patches/QuickSlots_Patch.cs
+++ MoreQuickSlotsBepInEx/Patches/QuickSlots_Patch.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 namespace MoreQuickSlotsBepInEx.Patches
 {
     [HarmonyPatch(typeof(QuickSlots))]
-    internal class QuickSlots_Patch
+    public class QuickSlots_Patch
     {
         [HarmonyPatch(MethodType.Constructor)]
         [HarmonyPatch(new Type[] { typeof(GameObject), typeof(Transform), typeof(Transform), typeof(Inventory), typeof(Transform), typeof(int) })]
@@ -18,6 +18,28 @@ namespace MoreQuickSlotsBepInEx.Patches
             for (int i = 0; i < slotCount; i++) { mySlotNames[i] = "QuickSlot" + i.ToString(); }
 
             typeof(QuickSlots).GetField("slotNames", BindingFlags.Static | BindingFlags.NonPublic).SetValue(null, mySlotNames);
+        }
+
+        public static void ReDrawSlots()
+        {
+            Inventory inv = Inventory.main;
+            if (inv == null)
+                return;
+
+            // Save slot bindings and selected slot to reapply (if possible)
+            InventoryItem[] oldBinding = inv.quickSlots.binding;
+            int oldDesiredSlot = inv.quickSlots.desiredSlot;
+
+            inv.quickSlots.DeselectImmediate();
+            // Rely on Prefix to handle adding more slots, next line mimics vanilla SN
+            inv.quickSlots = new QuickSlots(inv.gameObject, inv.toolSocket, inv.cameraSocket, inv, inv.GetComponent<Player>().rightHandSlot, 5);
+
+            // Reapply slot bindings and selected
+            for (int i = 0; (i < oldBinding.Length) && (i < inv.quickSlots.binding.Length); i++)
+                inv.quickSlots.binding[i] = oldBinding[i];
+
+            if (oldDesiredSlot <= inv.quickSlots.slotCount)
+                inv.quickSlots.Select(oldDesiredSlot);
         }
     }
 }
